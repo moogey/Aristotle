832eb9ddbd598d0b0817ed51d7efefa49ea5ea6d
joshuajgit <43454615+joshuajgit@users.noreply.github.com>
2019-10-23 22:31:51 -0400

    Save system with PlayerPrefs
    For now, there's a test scene called SaveSystemTest, since no levels are done yet from what I know.
    The buttons in the scene are placeholders for when levels will be cleared, and the load last level button will remember which of the other 4 buttons was clicked.

---
+++ Assets/CodeBase/UI/SaveSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveSystem : MonoBehaviour {
+
+    //to be used with the buttons
+    public void NewGame() {
+        PlayerPrefs.SetInt("aristotle_save", 0); //a save value of 0 will mean new game
+        PlayerPrefs.Save();
+    }
+
+    public void SimulateLevelClear(int whichLevel) {
+        PlayerPrefs.SetInt("aristotle_save", whichLevel);
+        PlayerPrefs.Save();
+    }
+
+    //load the according level/scene
+    public void LoadLastSave() {
+        int targetScene = PlayerPrefs.GetInt("aristotle_save") + 1;
+
+        print("We would begin level " + targetScene);
+    }
+}
